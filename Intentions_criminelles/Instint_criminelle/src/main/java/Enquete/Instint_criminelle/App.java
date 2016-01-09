04f3d5f75f9dfc868ca21d4eea3f67fec7bbbbb3
Thibaut <thibaut-mahe@orange.fr>
2016-01-09 12:52:05 +0100

    Nouvel emplacmeent de travail

---
+++ Intentions_criminelles/Instint_criminelle/src/main/java/Enquete/Instint_criminelle/App.java
@@ -0,0 +1,13 @@
+package Enquete.Instint_criminelle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
